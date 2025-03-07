4a32e7ed505159169e5d2a85d0ffcc511eff7d8f
Hector Acevedo <HEA092024@gmail.com>
2025-03-07 11:00:42 +0100

    added About.vue to component folder

---
+++ src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>About Me</h2>
+    <p>I am a web developer passionate about building amazing projects with Vue.js!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+};
+</script>
+
+<style scoped>
+h2 {
+  color: #42b983;
+}
+</style>
