e45178fa3de3085386eb4dd90ef3f167565c963d
Hector Acevedo <HEA092024@gmail.com>
2025-03-13 19:07:31 +0100

    added more info to about me

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -5,10 +5,7 @@
             Hello My name is Hector, I'm a 41 years young Frontend Developer. I started learning web development in september 2024 by attending a course from JobLoop that is called "KodeHode".
           </p>
           <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
-            totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
-            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, 
-            sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+            I’m currently attending a course at Jobloop to further expand my skills, and while I’m still learning, I’m eager to collaborate on projects or take on solo work related to design and development. I’m committed to putting in the hours needed to improve my coding abilities and create high-quality, impactful work.
           </p>
           <p>
             Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
