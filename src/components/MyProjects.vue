8428372d77177c08fd0a11950c4806ececcd18c5
Hector Acevedo <HEA092024@gmail.com>
2025-03-13 19:15:55 +0100

    added nav links to MainPage

--- src/components/MyProjects.vue
+++ src/components/MyProjects.vue
@@ -2,6 +2,21 @@
   <div>
     <h2>My Projects</h2>
   </div>
+
+   <!-- Navigation -->
+  <nav class="navbar">
+    <a href="#home" class="logo">Hector`s Portfolio</a>
+    <div class="hamburger" onclick="toggleNav()">
+      <i class="fas fa-bars"></i>
+    </div>
+    <ul class="nav-links">
+      <li><a href="#home">Home</a></li>
+      <li><a href="#skills">Skills</a></li>
+      <li><a href="#projects">Projects</a></li>
+      <li><a href="contact.html">Contact</a></li>
+      <li><a href="aboutme.html">About Me</a></li>
+    </ul>
+  </nav>
 </template>
 
 <script>
