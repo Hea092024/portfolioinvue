6289e90f043dfd44b4c53a1611ae928c4e6a7af2
Hector Acevedo <HEA092024@gmail.com>
2025-03-13 19:06:50 +0100

    updated contact info + added info to about me

--- src/components/ContactPage.vue
+++ src/components/ContactPage.vue
@@ -1,7 +1,26 @@
 <template>
-  <div>
-    <h2>Contact Me</h2>
-  </div>
+     <section id="contact" class="contact-page">
+      <div class="contact-container">
+        <h2>Contact Me</h2>
+        <div class="contact-details">
+          <div class="contact-info">
+            <h3>Hector Acevedo</h3>
+            <div class="contact-item">
+              <i class="fas fa-envelope"></i>
+              <span>ilalo1983@gmail.com</span>
+            </div>
+            <div class="contact-item">
+              <i class="fas fa-phone"></i>
+              <span>+47 93 46 22 76 </span>
+            </div>
+            <div class="contact-item">
+              <i class="fas fa-map-marker-alt"></i>
+              <span>Bergen, Norway</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
 </template>
 
 <script>
@@ -13,5 +32,14 @@ export default {
 <style scoped>
 h2 {
   color: #42b983;
+}
+
+.contact-page {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 5rem 5%;
+  background: green;
 }
 </style>
